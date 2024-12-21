337407ef72ad66d2f0de26ae26bda71845250705
jaunger1999 <92285971+jaunger1999@users.noreply.github.com>
2024-12-21 06:49:29 -0600

    more tetris functionality

--- tetris.js
+++ tetris.js
@@ -1,49 +1,43 @@
-// different piece patterns
-const l = [
-	[ true,	true,	true,	true, ],
-	[ false, false, false, false, ],
-]
-
-const t = [
-	[ false,	true,	true,	true, ],
-	[ false, false, true, false, ],
-]
-
-const L = [
-	[ false,	true,	true,	true, ],
-	[ false, true, false, false, ],
-]
-
-const reverseL = [
-	[ false,	true,	true,	true, ],
-	[ false, false, false, true, ],
-]
-
-const square = [
-	[ false,	true,	true,	false, ],
-	[ false, true, true, false, ],
-]
-
-const zig = [
-	[ false,	true,	true,	true, ],
-	[ true, true, false, false, ],
-]
-
-const zag = [
-	[ true,	true,	true,	false, ],
-	[ false, true, false, true, ],
-]
+// different piece patterns and their starting positions
+const l = [[3, -1], [4, -1], [5, -1], [6, -1],]
+const t = [[4, -1], [5, -1], [5, 0], [6, -1],]
+const L = [[4, 0], [4, -1], [5, -1], [6, -1],]
+const reverseL = [[4, -1], [5, -1], [6, -1], [6, 0],]
+const square = [[4, -1], [4, 0], [5, -1], [5, 0],]
+const zig = [[4, -1], [5, -1], [5, 0], [6, 0],]
+const zag = [[4, 0], [5, 0], [5, -1], [6, -1],]
 
 const pieces = [
 	l, t, L, reverseL, square, zig, zag,
 ]
 
-let currPiece = new Array(4);
+const board = document.getElementById("board")
+const height = board.rows.Length
+
+// coordinates of the spaces our piece is occupying
+let currPiece = [[0, 0], [0, 0], [0, 0], [0, 0],]
 let millisecondsPerTic = 500
 let gameTicID
+let gameOver = false
+let level = 1
 
 function movePieceDown() {
+	// if there's a piece below our current piece... 
+	if (currPiece.some((xy) => board.rows[xy[1]].cells[xy[0] + 1].classList.contains("piece"))) {
+		// ...and we're still above the top of the grid, set gameOver and return.
+		if (currPiece.some((xy) => xy[1] < 0)) {
+			gameOver = true
+			return
+		}
 
+		// set a new piece
+		currPiece = pieces[Math.floor(Math.random(pieces.length))]
+
+		// check for cleared lines
+		return
+	}
+
+	currPiece.forEach((xy) => xy[1]++)
 }
 
 gameTicID = setInterval(movePieceDown, millisecondsPerTic)
